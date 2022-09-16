cb35ad9160c1c3e4ca9307a790abc1af8e8264ae
malcolm maima <malcolmmaima@gmail.com>
2022-09-16 08:45:38 +0300

    Setup Core

---
+++ core/build.gradle.kts
@@ -0,0 +1,60 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("com.hiya.jacoco-android")
+}
+
+jacoco {
+    toolVersion = "0.8.4"
+}
+
+android {
+
+    compileSdk = 31
+
+    defaultConfig {
+        minSdk = 21
+        targetSdk = 31
+        vectorDrawables.useSupportLibrary = true
+    }
+
+    testOptions {
+        animationsDisabled = true
+        unitTests.apply {
+            isReturnDefaultValues = true
+            isIncludeAndroidResources = false
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+    buildTypes {
+        getByName("debug") {
+            isMinifyEnabled = false
+        }
+
+        getByName("release") {
+            isMinifyEnabled = true
+            proguardFiles("proguard-android.txt", "proguard-rules.pro")
+        }
+    }
+}
+
+dependencies {
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+    implementation(Libraries.kotlinStandardLib)
+    implementation(Libraries.ktx)
+
+    // For Prefix Transformation View Utility
+    implementation("androidx.compose.ui:ui-text:1.0.1")
+
+    // timber
+    implementation(Libraries.timber)
+}
